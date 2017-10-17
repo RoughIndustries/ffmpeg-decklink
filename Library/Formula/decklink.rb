c334e355efcaa789cbec538d6b4255ba3f6ca60e
Rough Industries LLC <travishdc@roughindustries.com>
2017-10-17 11:33:06 +0200

    Update decklink.rb

--- Library/Formula/decklink.rb
+++ Library/Formula/decklink.rb
@@ -3,6 +3,6 @@ class Decklink < Formula
   url "https://github.com/RoughIndustries/ffmpeg-decklink/raw/master/DeckLinkAPI_10.8.3.tgz"
   sha256 "f0898a9fcda718b2632ce49889535365721f1bd5900293947bf11a715a68d0b5"
   def install
-    include.install Dir["include/*"]
+    include.install Dir["*"]
   end
 end
